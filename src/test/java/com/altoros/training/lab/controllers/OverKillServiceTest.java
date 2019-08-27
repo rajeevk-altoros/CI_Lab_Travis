e811b0ba9154ce072db9a1126af0876a0d325670
rajeev kuruganti <rajeev.kuruganti@altoros.com>
2019-08-27 14:47:58 -0400

    updated manifest.yml

--- src/test/java/com/altoros/training/lab/controllers/OverKillServiceTest.java
+++ src/test/java/com/altoros/training/lab/controllers/OverKillServiceTest.java
@@ -21,7 +21,7 @@ public class OverKillServiceTest {
     public void testNoValue(){
 
         String actual = "Hello Buttercup";
-        String result = overKillService.concatenate2("");
+        String result = overKillService.concatenate2("cup");
         assertNotEquals (actual, result);
 
     }
