87dfd99a9e1fe2322dfb8101bd4eb9d916a7d464
Rajeev Kuruganti <rajeev.kuruganti@altoros.com>
2019-08-27 14:16:09 -0400

    modified some text in test

--- src/test/java/com/altoros/training/lab/controllers/OverKillServiceTest.java
+++ src/test/java/com/altoros/training/lab/controllers/OverKillServiceTest.java
@@ -6,13 +6,23 @@ import static org.junit.Assert.*;
 
 public class OverKillServiceTest {
 
-    @Test
-    public void testConcatenate2(){
+    OverKillService overKillService = new OverKillService();
 
-        OverKillService overKillService = new OverKillService();
+    @Test
+    public void testkHappyPath(){
+
         String actual = "Hello Inigo";
         String result = overKillService.concatenate2("Inigo");
         assertEquals(actual, result);
+
+    }
+
+    @Test
+    public void testNoValue(){
+
+        String actual = "Hello Buttercup";
+        String result = overKillService.concatenate2("NotButter");
+        assertNotEquals (actual, result);
 
     }
 
